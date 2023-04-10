0b5b24c4b9cfc03513ba35af1599ba1ba9dfc932
AbulrhamnMohamed <126196441+AbulrhamnMohamed@users.noreply.github.com>
2023-04-10 19:53:11 +0200

    return scaffold

--- lib/responsive/mobile.dart
+++ lib/responsive/mobile.dart
@@ -10,6 +10,6 @@ class MobileScreen extends StatefulWidget {
 class _MobileScreenState extends State<MobileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
