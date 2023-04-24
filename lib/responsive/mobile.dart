819ac0a9e491b68f209f5ff25d6b486e0fe5ceb1
AbulrhamnMohamed <126196441+AbulrhamnMohamed@users.noreply.github.com>
2023-04-24 19:08:09 +0200

    remove mode banner

--- lib/responsive/mobile.dart
+++ lib/responsive/mobile.dart
@@ -14,6 +14,7 @@ class _MobileScreenState extends State<MobileScreen> {
       appBar: AppBar(
         title: Text("Mobile screen "),
       ),
+      bottomNavigationBar: BottomNavigationBar(),
     );
   }
 }
