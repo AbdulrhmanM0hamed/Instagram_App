dcc29863e0a2cc00b21e350548e66fc6bff2151a
AbulrhamnMohamed <126196441+AbulrhamnMohamed@users.noreply.github.com>
2023-04-24 23:53:33 +0200

    add profile page to childeren of page view

--- lib/responsive/mobile.dart
+++ lib/responsive/mobile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram/screens/add_post.dart';
 import 'package:instagram/screens/home.dart';
+import 'package:instagram/screens/profile.dart';
 import 'package:instagram/screens/search.dart';
 
 class MobileScreen extends StatefulWidget {
@@ -56,11 +57,7 @@ class _MobileScreenState extends State<MobileScreen> {
       ),
       body: PageView(
         onPageChanged: (index) {},
-        children: [
-          Home(),
-          Search(),
-          Add_post(),
-        ],
+        children: [Home(), Search(), Add_post(), Profile()],
       ),
     );
   }
