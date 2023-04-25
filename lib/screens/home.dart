558ec309711cfc5432e0103b62ec357773b48ff2
AbulrhamnMohamed <126196441+AbulrhamnMohamed@users.noreply.github.com>
2023-04-25 12:57:19 +0200

    change background color of bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,6 +8,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.black,
         title: SvgPicture.asset(
           "assets/img/instagram.svg",
           color: Colors.white,
