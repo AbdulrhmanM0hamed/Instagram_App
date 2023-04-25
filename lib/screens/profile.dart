a6c3bee045b831c8a35ca0a8e0a3d14783393406
AbulrhamnMohamed <126196441+AbulrhamnMohamed@users.noreply.github.com>
2023-04-25 19:56:10 +0200

    add radius his value 40 and image

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -15,12 +15,19 @@ class Profile extends StatelessWidget {
           Row(
             children: [
               Container(
-                  margin: EdgeInsets.only(left: 22),
-                  padding: EdgeInsets.all(4),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Color.fromARGB(125, 78, 91, 110),
-                  )),
+                margin: EdgeInsets.only(left: 22),
+                padding: EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color.fromARGB(125, 78, 91, 110),
+                ),
+                child: CircleAvatar(
+                  radius: 40,
+                  backgroundImage: NetworkImage(
+                      // widget.snap["profileImg"],
+                      "https://i.pinimg.com/564x/94/df/a7/94dfa775f1bad7d81aa9898323f6f359.jpg"),
+                ),
+              ),
             ],
           )
         ],
