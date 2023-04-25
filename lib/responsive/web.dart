d443a6e8e2a03665ba1f6e2bedb155b370692fdb
AbulrhamnMohamed <126196441+AbulrhamnMohamed@users.noreply.github.com>
2023-04-25 14:22:57 +0200

    add app bar to web

--- lib/responsive/web.dart
+++ lib/responsive/web.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:instagram/screens/add_post.dart';
+import 'package:instagram/screens/favorite.dart';
+import 'package:instagram/screens/home.dart';
+import 'package:instagram/screens/profile.dart';
+import 'package:instagram/screens/search.dart';
 
 class WebScreen extends StatefulWidget {
   const WebScreen({super.key});
@@ -12,7 +17,65 @@ class _WebScreenState extends State<WebScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Web screen"),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.home,
+              color: Color.fromARGB(255, 76, 75, 75),
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: secondaryColor,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.add_a_photo,
+              color: secondaryColor,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.favorite,
+              color: secondaryColor,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.person,
+              color: secondaryColor,
+            ),
+            onPressed: () {},
+          ),
+        ],
+        backgroundColor: mobileBackgroundColor,
+        title: SvgPicture.asset(
+          "assets/img/instagram.svg",
+          color: primaryColor,
+          height: 32,
+        ),
+      ),
+      body: PageView(
+        onPageChanged: (index) {
+         
+        },
+        physics: NeverScrollableScrollPhysics(),
+        // controller: _pageController,
+        children: [
+          Home(),
+          Search(),
+          Add_post(),
+          Center(child: Text("Love u ♥")),
+          Favorite(),
+          Profile(),
+        ],
+      ),
       ),
     );
   }
