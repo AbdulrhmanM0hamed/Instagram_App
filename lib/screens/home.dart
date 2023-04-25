e4969e41eefc3f0d1342c4a59b89480ee31e97e8
AbulrhamnMohamed <126196441+AbulrhamnMohamed@users.noreply.github.com>
2023-04-25 13:54:45 +0200

    add new icon down  the picture

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -57,6 +57,14 @@ class Home extends StatelessWidget {
                   image: DecorationImage(
                       image: NetworkImage(
                           "https://th.bing.com/th/id/R.3a9f0210ff7d1e83039ff237c81bacae?rik=asArcL6qvWfZfA&riu=http%3a%2f%2fs1.picswalls.com%2fwallpapers%2f2016%2f03%2f29%2fbeautiful-nature-high-definition_042323787_304.jpg&ehk=8tO7UxFL%2bC03x6vfc2O9EhdUCe4fl6tC7UGUdlnpdX4%3d&risl=&pid=ImgRaw&r=0"))),
+            ),
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.favorite_outline),
+                ),
+              ],
             )
           ],
         ),
